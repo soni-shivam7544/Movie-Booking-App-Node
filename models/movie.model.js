8f674446f9a86a499f7366a0a30ec28cbde71807
soni-shivam7544 <sonishivam7544@gmail.com>
2025-11-04 23:33:07 +0530

    Feature: movie model created

---
+++ models/movie.model.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    casts: {
+        type: [String],
+        required: true
+    },
+    trailerUrl: {
+        type: String,
+        required: true
+    },
+    language: {
+        type: String,
+        required: true,
+        default: "English"
+    },
+    releaseDate: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    releaseStatus: {
+        type: String,
+        required: true,
+        default: "Released"
+    },
+}, { timestamps: true });
+
+const Movie = mongoose.model('Movie', movieSchema);
+module.exports = Movie;
