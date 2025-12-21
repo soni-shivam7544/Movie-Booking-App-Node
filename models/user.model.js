f4ac239889f7cdd80329a9c92f91b3a593b386f1
soni-shivam7544 <sonishivam7544@gmail.com>
2025-12-21 10:20:53 +0530

    Feat: User schema created

---
+++ models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Please fill a valid email address'],
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
+    },
+    userRole: {
+        type: String,
+        required: true,
+        default: 'CUSTOMER'
+    },
+    userStatus: {
+        type: String,
+        required: true,
+        default: 'APPROVED'
+    }
+}, {timestamps: true});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
